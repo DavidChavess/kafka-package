3e622c0f7d576cd0c1e568d402f039d4de20888f
DavidChavess <davi.ch.fe@gmail.com>
2023-07-31 19:39:01 -0300

    chore: release 1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.davidchavess"
             artifactId = "kafka-package"
-            version = "1.4.1"
+            version = "1.4.2"
             from(components["java"])
         }
     }
