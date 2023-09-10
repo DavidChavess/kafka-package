be2c6f96544068ff47d99def34e8e0e1b7071655
DavidChavess <davi.ch.fe@gmail.com>
2023-09-10 19:41:18 -0300

    feat: add name and email in Order class

--- src/main/kotlin/domain/Order.kt
+++ src/main/kotlin/domain/Order.kt
@@ -2,4 +2,10 @@ package domain
 
 import java.math.BigDecimal
 
-data class Order(val userId: String, val orderId: String, val total: BigDecimal)
+data class Order(
+    val userId: String,
+    val orderId: String,
+    val total: BigDecimal,
+    val name: String,
+    val email: String
+)
